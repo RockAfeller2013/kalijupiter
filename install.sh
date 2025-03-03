b4d6a2567e1194e7d8650654ce7cae38f700b923
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-03-03 15:34:48 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 # sudo curl -sL https://raw.githubusercontent.com/RockAfeller2013/kalijupiter/main/install.sh | bash
 sudo apt update && sudo apt install jupyter-notebook -y
 pip install bash_kernel
 python3 -m bash_kernel.install
-jupyter-notebook
+wget -O notebook.ipynb "https://raw.githubusercontent.com/obheda12/JupyterPen/blob/main/OSINT/Jupyter_OSINT.ipynb" && jupyter-notebook notebook.ipynb

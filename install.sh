2ce01b1be0abe19fad168faa0801b362143444ec
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-03-03 15:22:17 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # sudo curl -s https://github.com/RockAfeller2013/kalijupiter/blob/main/install.sh | bash
+# sudo curl -sL https://raw.githubusercontent.com/RockAfeller2013/kalijupiter/main/install.sh | bash
 sudo apt update && sudo apt install jupyter-notebook -y
 pip install bash_kernel
 python3 -m bash_kernel.install

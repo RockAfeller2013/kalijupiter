69b87c9647e9cb23be240172c3b3ead3ee87f75e
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-03-04 10:41:32 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,32 +23,13 @@ sudo apt install -y python3-pip python3-venv zip certbot
 pip3 install --break-system-packages jupyter pandas openpyxl bash_kernel 
 python3 -m bash_kernel.install
 
-
-# Install Anaconda
-# The command sudo apt install -y anaconda will not work in Kali Linux because Anaconda is not available in the default APT repositories.
-# https://www.anaconda.com/docs/getting-started/anaconda/install#macos-linux-installation
-sudo curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
-echo yes | sudo bash Anaconda3-2024.10-1-Linux-x86_64.sh -b -p $HOME/anaconda3
-source ~/.bashrc
-
-# Replace <PATH_TO_CONDA> with the path to your conda install
-source <PATH_TO_CONDA>/bin/activate
-conda init --all
-conda config --set auto_activate_base True
-
-# bash anaconda.sh -b -p $HOME/anaconda3
-echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-anaconda-navigator
-
-
 # Verify Installation
 
 jupyter --version
 python3 -c "import pandas; print(pandas.__version__)"
 python3 -c "import openpyxl; print(openpyxl.__version__)"
 jupyter kernelspec list
-conda --version  # Only if Anaconda is installed
+
 
 # Setyp Jupiter
 mkdir /root/.jupyter/ssl

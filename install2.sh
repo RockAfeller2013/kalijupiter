3e81fbc8792182faf7394f462afb5f2a33223751
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-03-06 13:54:11 +1000

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -6,7 +6,9 @@ echo "Updating system..."
 sudo apt update && sudo apt upgrade -y
 
 # Install Tools
-sudo apt-get install -y crackmapexec nmap nbtscan enum4linux samba-common-bin smbclient enum4linux enum4linux-ng krb5-user python3-impacket impacket-scripts
+sudo apt-get install -y crackmapexec nmap nbtscan enum4linux samba-common-bin smbclient enum4linux enum4linux-ng krb5-user python3-impacket
+sudo apt install impacket-scripts -y
+
 
 # Install Basics
 echo "Installing basics..."
